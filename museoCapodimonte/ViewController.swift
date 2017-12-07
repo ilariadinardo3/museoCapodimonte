46bca088f0070df0e3970edef0eb2110260b0565
martin_e91 <martin.essuman@gmail.com>
2017-12-07 10:51:49 +0100

    aggiunta print in viewcontroller

--- museoCapodimonte/ViewController.swift
+++ museoCapodimonte/ViewController.swift
@@ -18,8 +18,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        print("Hello, world")
     }
 
-
+    
 }
 
